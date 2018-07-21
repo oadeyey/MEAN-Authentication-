333a3ea41072c0b735d9b39056fff06b2c77ce74
Tobi Adeyeye <oadeyey@okstate.edu>
2018-07-21 10:44:10 -0500

    committed

--- app.js
+++ app.js
@@ -24,13 +24,14 @@ const app = express();
 const users = require('./routes/users');
 
 // Port Number
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 //cors middleware
 app.use(cors());
 
 //Set static file
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Body Parser MiddleWare
 app.use(bodyParser.json());
